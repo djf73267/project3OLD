6aa849c6bf7e0cee53179985ad534d532d4f9bce
DARREN FUNES <funes@nike.cs.uga.edu>
2018-07-23 21:40:52 -0400

    second update

--- newer.cpp
+++ newer.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+1;95;0c#include <iostream>
 #include <cstring>
 #include <unistd.h>
 #include <fcntl.h>
@@ -39,36 +39,49 @@ int main(int argc, char * argv[]) {
     case 'a':
       //for loop that will start at the first file argument and iterate through to the second argument 
       for(int i = 2; i < argc; i++){
-	//if conditional to determine if for loop iteration is the first and not the second 
+	//if conditional to determine if the for loop iteration is the first and not the second iteration because without it
+	//it will run through the second argument and uodate the file descriptor 
 	if(i == 2){
+   //creates a file descriptor for the first file argument from the system call open
 	  fd1 = open (argument[i],O_RDONLY);
+	  //perror conditional if the file descriptor returns -1 
 	  if(fd1 == -1){
 	    perror("Doesn't exist");
 	    exit(1);
 	  }//if
+	  //using the fstat function with the values within values from stored variables with file descriptor 1 and the buffer
 	  n1 = fstat(fd1, &buf1);
+	  //perror conditional if the file descriptor returns -1 
 	  if (n1==-1){
 	    cout << "error" << endl;
 	    exit(1);
 	  }
+	  //calls the recently accessed from the buf1 and returns file1Time 
 	  int file1Time = buf1.st_atime; 
 	}//if
 	
+ //if conditional to determine if the for loop iteration is the second and not the first iteration because without it
+	//it will run through the first argument and uodate the file descriptor 
 	if(i == 3){
+ //creates a file descriptor for the second file argument from the system call open
 	  fd2 = open (argument[i],O_RDONLY);
+     //perror conditional if the file descriptor returns -1 
 	  if(fd2 == -1){
 	    perror("Doesn't exist");
 	    exit(1);
 	  }//if
+     //using the fstat function with the values within values from stored variables with file descriptor 1 and the buffer
 	  n2 = fstat(fd2, &buf2);
+    //perror conditional if the file descriptor returns -1 
 	  if (n2==-1){
 	    cout << "error" << endl;
 	    exit(2);
 	  }
+     //calls the recently accessed from the buf1 and returns file1Time 
 	  int file2Time = buf1.st_atime; 
 	}//if
       }//for
-
+      //an three if conditional statements for if one file argument is larger than the other one or if equal
       if(buf1.st_atime > buf2.st_atime){
 	cout <<argument[2] <<" was most recently accessed." << endl;
       }else if(buf1.st_atime == buf2.st_atime){
@@ -76,31 +89,44 @@ int main(int argc, char * argv[]) {
       } else{
 	cout <<argument[3] <<" was most recently accessed." << endl;
       }
+      //will break the loop after
       break;//case a 
 
     case 'm':
+    //for loop that will start at the first file argument and iterate through to the second argument 
       for(int i = 2; i < argc; i++){
+      //if conditional to determine if the for loop iteration is the first and not the second iteration because without it
+	//it will run through the second argument and uodate the file descriptor 
 	if(i == 2){
+	 //creates a file descriptor for the first file argument from the system call open
 	  fd1 = open (argument[i],O_RDONLY);
+	  //perror conditional if the file descriptor returns -1 
 	  if(fd1 == -1){
 	    perror("Doesn't exist");
 	    exit(1);
 	  }//if
+	  //using the fstat function with the values within values from stored variables with file descriptor 1 and the buffer
 	  n1 = fstat(fd1, &buf1);
+	  //perror conditional if the file descriptor returns -1 
 	  if (n1==-1){
 	    cout << "error" << endl;
 	    exit(1);
 	  }
 	  int file1Time = buf1.st_mtime; 
 	}//if
-	
+	//if conditional to determine if the for loop iteration is the second and not the first iteration because without it
+	//it will run through the first argument and update the file descriptor 
 	if(i == 3){
+	  //creates a file descriptor for the second file argument from the system call open
 	  fd2 = open (argument[i],O_RDONLY);
+	  //perror conditional if the file descriptor returns -1 
 	  if(fd2 == -1){
 	    perror("Doesn't exist");
 	    exit(1);
 	  }//if
+	  //using the fstat function with the values within values from stored variables with file descriptor 1 and the buffer
 	  n2 = fstat(fd2, &buf2);
+	  //perror conditional if the file descriptor returns -1 
 	  if (n2==-1){
 	    cout << "error" << endl;
 	    exit(2);
@@ -108,7 +134,7 @@ int main(int argc, char * argv[]) {
 	  int file2Time = buf1.st_mtime; 
 	}//if
       }//for
-
+      //an three if conditional statements for if one file argument is larger than the other one or if equal
       if(buf1.st_mtime > buf2.st_mtime){
 	cout <<argument[2] <<" was most recently modified." << endl;
       }else if(buf1.st_mtime == buf2.st_mtime){
@@ -119,27 +145,38 @@ int main(int argc, char * argv[]) {
       break; //case m
     case 'c':
       for(int i = 2; i < argc; i++){
+      //if conditional to determine if the for loop iteration is the first and not the second iteration because without it
+	//it will run through the second argument and uodate the file descriptor 
 	if(i == 2){
+    //creates a file descriptor for the first file argument from the system call open
 	  fd1 = open (argument[i],O_RDONLY);
+     //perror conditional if the file descriptor returns -1 
 	  if(fd1 == -1){
 	    perror("Doesn't exist");
 	    exit(1);
 	  }//if
+     //using the fstat function with the values within values from stored variables with file descriptor 1 and the buffer
 	  n1 = fstat(fd1, &buf1);
+     //perror conditional if the file descriptor returns -1 
 	  if (n1==-1){
 	    cout << "error" << endl;
 	    exit(1);
 	  }
 	  int file1Time = buf1.st_ctime; 
 	}//if
-	
+	//if conditional to determine if the for loop iteration is the second and not the first iteration because without it
+	//it will run through the first argument and update the file descriptor 
 	if(i == 3){
+	  //creates a file descriptor for the second file argument from the system call open
 	  fd2 = open (argument[i],O_RDONLY);
+	  //perror conditional if the file descriptor returns -1 
 	  if(fd2 == -1){
 	    perror("Doesn't exist");
 	    exit(1);
 	  }//if
+	  //using the fstat function with the values within values from stored variables with file descriptor 1 and the buffer
 	  n2 = fstat(fd2, &buf2);
+	  //perror conditional if the file descriptor returns -1 
 	  if (n2==-1){
 	    cout << "error" << endl;
 	    exit(2);
@@ -147,7 +184,7 @@ int main(int argc, char * argv[]) {
 	  int file2Time = buf1.st_ctime; 
 	}//if
       }//for
-
+      //an three if conditional statements for if one file argument is larger than the other one or if equal
       if(buf1.st_ctime > buf2.st_ctime){
 	cout <<argument[2] <<" was most recently changed." << endl;
       }else if(buf1.st_ctime == buf2.st_ctime){
